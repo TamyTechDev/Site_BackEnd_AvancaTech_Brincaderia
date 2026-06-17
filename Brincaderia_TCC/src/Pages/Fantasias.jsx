501ad3790c85be4206db7eb00c9fb1a1be9ba2c0
Tamy T. T. <tamyt.tech@gmail.com>
2026-06-17 10:00:24 -0300

    atualização
    atualização  teste

---
+++ Brincaderia_TCC/src/Pages/Fantasias.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import Foto1 from "../img/Foto1.png";
+import Foto00 from "../img/Foto00.png";
+import Home1 from "../img/Home1.png";
+import Barradeservicos from "../img/Barradeservicos.png";
+import BarraPatrocinio from "../img/BarraPatrocinio.png";
+
+
+
+
+function Fantasias () {
+  return (
+    <>
+      <div className=" mt-2 ">
+       <h5 className=" fw-bold text-center  ">
+        ENVIAMOS PARA TODO BRASIL 8 % DE DESCONTO NO PIX
+        </h5>
+      </div>
+    
+
+    </>
+      
+ 
+
+    
+  );
+}
+
+
+export default Fantasias;
